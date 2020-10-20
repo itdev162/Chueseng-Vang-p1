8099f20c1dac1c0fa29ab44e41db4fb1aa60d649
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-10-20 12:52:10 -0500

    Updated Project 1

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water, place egg in water", Platform="Kettle & Pan"},
+                new Command{Id=1, HowTo="Cut Bread", Line="Get a knife", Platform="Knife and Chopping board"},
+                new Command{Id=2, HowTo="Make a cup of tea", Line="Boil water, hot water to cup, place teabag in cup,", Platform="Kettle & Cup"},
+            };
+
+            return commands;
+        } 
+ 
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water, place egg in water", Platform="Kettle & Pan"};
+        }
+    }
+}
