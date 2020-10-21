6b026d5216c032f38e323b141dbe67c384d1e3ce
Chueseng Vang <chewyvang@Chuesengs-MacBook-Pro.local>
2020-10-21 13:56:24 -0500

    Updated Project 1

--- Commander/Data/MockCommanderRepo.cs
+++ Commander/Data/MockCommanderRepo.cs
@@ -7,7 +7,7 @@ namespace Commander.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            var commands = new List<Command>        //Command datas 
+            var commands = new List<Command>        //Data in an Array
             {
                 new Command{Id=0, HowTo="Boil an egg", Line="Boil water, place egg in water", Platform="Kettle & Pan"},
                 new Command{Id=1, HowTo="Cut Bread", Line="Get a knife", Platform="Knife and Chopping board"},
